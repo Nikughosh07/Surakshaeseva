efa059eeded81ad1a46bb0713ea975494dc358ce
Niku Ghosh <147630574+Nikughosh07@users.noreply.github.com>
2026-02-26 02:05:07 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,11 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    // you can add other directories if components move elsewhere
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
